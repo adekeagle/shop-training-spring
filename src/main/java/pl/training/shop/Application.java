f00dcf8f72390fc82f1e1b2064dfcf89da363f93
adekeagle <adrian1230@wp.pl>
2021-09-20 22:52:50 +0200

    Initial commit

---
+++ src/main/java/pl/training/shop/Application.java
@@ -0,0 +1,18 @@
+package pl.training.shop;
+
+import lombok.extern.java.Log;
+import pl.training.shop.payments.*;
+
+@Log
+public class Application {
+    public static void main(String[] args) {
+        var paymentIdGenerator = new UUIDPaymentIdGenerator();
+        var paymentService = new FakePaymentService(paymentIdGenerator);
+        var paymentRequest = PaymentRequest.builder()
+                .money(LocalMoney.of(1_0000))
+                        .build();
+
+        var payment = paymentService.process(paymentRequest);
+        log.info(payment.toString());
+    }
+}
---
+++ src/main/java/pl/training/shop/payments/FakePaymentService.java
@@ -0,0 +1,34 @@
+package pl.training.shop.payments;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+
+import java.time.Instant;
+
+@Log
+@RequiredArgsConstructor
+public class FakePaymentService {
+
+    public static final String LOG_FORMAT = "A new payment of %s has been initiated";
+
+    private final UUIDPaymentIdGenerator paymentIdGenerator;
+
+    public Payment process(PaymentRequest paymentRequest){
+        var payment = Payment.builder()
+                .id(paymentIdGenerator.getNext())
+                .money(paymentRequest.getMoney())
+                .timestamp(Instant.now())
+                .status(PaymentStatus.STARTED)
+                .build();
+
+        log.info(createLogEntry(payment));
+
+        return payment;
+    }
+
+
+    public String createLogEntry(Payment payment){
+        return String.format(LOG_FORMAT, payment.getMoney());
+    }
+
+}
